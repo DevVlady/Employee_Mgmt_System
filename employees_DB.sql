06b0b93fe0a86f59f42c8a665ce32065de49584b
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-02 00:38:25 -0600

    Installed all neccessary packages and created emplyees.js file.

--- employees_DB.sql
+++ employees_DB.sql
@@ -9,17 +9,20 @@ CREATE TABLE employees (
     last_name VARCHAR(50) NOT NULL,
     role_id INT NULL,
     manager_id INT NULL
+    PRIMARY KEY (first_name)
 );
 
 CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL,
-)
+    PRIMARY KEY (department_name)
+);
 
 CREATE TABLE employee_role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 4) NULL,
     department_id INT NULL,
-)
+    PRIMARY KEY (title)
+);
 
 SELECT * FROM employees;
 SELECT * FROM department_name;
